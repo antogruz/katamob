fe74f5ca132ed93d2e8da3677e1347e972bc7e1c
Antoine Gruzelle <agruzelle@salesforce.com>
2020-11-24 15:26:25 +0100

    refactor

--- FizzBuzz.test.js
+++ FizzBuzz.test.js
@@ -3,21 +3,25 @@ function fizzbuzz(input) {
 };
 
 describe("fizzbuzz", function () {
-  it("should return 1", function () {
-    // call fizz buzz
-    const expected = fizzbuzz(1);
-    expect(expected).toBe("1");
-  });
+    describe("Without matching any rule, returns input", function () {
 
-  it("should return 2", function () {
-    // call fizz buzz
-    const expected = fizzbuzz(2);
-    expect(expected).toBe("2");
-  });
+        it("should return 1", function () {
+            // call fizz buzz
+            const expected = fizzbuzz(1);
+            expect(expected).toBe("1");
+        });
 
-  it("should return Fizz when number is a multiple of 3", function(){
-    const expected = fizzbuzz(3);
-    expect(expected).toBe("Fizz");
-  });
+        it("should return 2", function () {
+            // call fizz buzz
+            const expected = fizzbuzz(2);
+            expect(expected).toBe("2");
+        });
+    });
 
+    describe("should return Fizz when number is a multiple of 3", function(){
+        it("with 3", function() {
+            const expected = fizzbuzz(3);
+            expect(expected).toBe("Fizz");
+        });
+    });
 });
