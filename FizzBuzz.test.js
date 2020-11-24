5324c62d44eb9d207a9368f60fe0715ce9fb562f
Antoine Rosenbach <arosenbach@salesforce.com>
2020-11-24 14:58:46 +0100

    first green test

--- FizzBuzz.test.js
+++ FizzBuzz.test.js
@@ -1,10 +1,12 @@
+function fizzbuzz(input) {
+    return "1";
+};
 
 
-describe('fizzbuzz', function () {
-    it('should return 1', function () {
-
-        // call fizz buzz
-
-        expect(1).toBe(1);
-    });
+describe("fizzbuzz", function () {
+  it("should return 1", function () {
+    // call fizz buzz
+    const expected = fizzbuzz(1);
+    expect(expected).toBe("1");
+  });
 });
